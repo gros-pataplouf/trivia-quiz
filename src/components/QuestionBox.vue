dc1afb802921e97ea68bcedf833f02813eb7a1ce
gros-pataplouf <sceres@pm.me>
2024-11-19 19:52:03 +0100

    add vuetify

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,7 +1,23 @@
 <template>
+    <v-container>
 
-    <h2>
-       Question Box!
-    </h2>
-
+        <v-card class="mx-auto" max-width="344" hover>
+            <v-card-item>
+                <v-card-title>
+                    Card title
+                </v-card-title>
+                <v-list-item v-for="n in 3" :key="n">{{ n }}</v-list-item>
+            </v-card-item>
+            <v-container>
+            <v-row>
+                <v-col align="center">
+                    <v-btn color="primary">Submit</v-btn>
+                </v-col>
+                <v-col align="end">
+                    <v-btn color="success">Next</v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+        </v-card>
+    </v-container>
 </template>
--- src/components/QuizHeader.vue
+++ src/components/QuizHeader.vue
@@ -1,3 +1,12 @@
 <template>
-    <h1>Header</h1>
+    <v-container>
+        <v-app-bar :elevation="2" color="warning">
+            <v-col>
+                <v-app-bar-title>Quiz Time</v-app-bar-title>
+            </v-col>
+            <v-col>
+                <v-app-bar-title>Counter: 4/10</v-app-bar-title>
+            </v-col>
+        </v-app-bar>
+    </v-container>
 </template>
